7ecab8ed33618dce36a28761164c177b9a8f5635
AmanRai145 <amanrai0745@gmail.com>
2024-08-07 00:58:17 +0530

    pushed the repo

---
+++ app/componentes/rent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+
+    <div className='bg-black text-white p-5 text-center font-bold'>header
+    </div>
+  )
+}
+
+export default page
